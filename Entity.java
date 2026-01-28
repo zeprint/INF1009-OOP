d7e5739dbd45c4bf609d0492df0bc725d09dc400
bransonnng <2500678@sit.singaporetech.edu.sg>
2026-01-28 18:27:20 +0800

    Add files via upload

---
+++ Entity.java
@@ -0,0 +1,102 @@
+package io.github.some_example_name.lwjgl3;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+
+//Default functions for now, remove or add any if needed
+public class Entity{ //rename any variable if needed
+    protected float posX;
+    protected float posY; 
+    protected Color color;
+    protected float speedX;
+    protected float speedY;
+    protected SpriteBatch batch;
+    
+    public float getX() {
+        return posX;
+    }
+    public void setX(float x) {
+        posX = x;
+    }
+    
+    public float getY() {
+        return posY;
+    }
+    public void setY(float y) {
+        posY = y;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+    public void setColor(Color c) {
+        color = c;
+    }
+    
+    public float getSpeedX() {
+        return speedX;
+    }
+    public void setSpeedX(float spdX) {
+        speedX = spdX;
+    }
+    
+    public float getSpeedY() {
+        return speedY;
+    }
+    public void setSpeedY(float spdY) {
+        speedY = spdY;
+    }
+    
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+    public void setBatch(SpriteBatch b) {
+        batch = b;
+    }
+    
+    public void draw(ShapeRenderer shape) {
+
+    }
+    
+    /*public void movement(float speed) {
+
+    }*/
+    
+    public Entity() {
+        posX = 0f;
+        posY = 0f;
+        color = null;
+        speedX = 0f;
+        speedY = 0f;
+        batch = null;
+    }
+    
+    public Entity(float x, float y) {
+        posX = x;
+        posY = y;
+        color = null;
+        speedX = 0f;
+        speedY = 0f;
+        batch = null;
+    }
+    
+    public Entity(float x, float y, Color c) {
+        posX = x;
+        posY = y;
+        color = c;
+        speedX = 0f;
+        speedY = 0f;
+        batch = null;
+    }
+    
+    public Entity(float x, float y, Color c, float spdX, float spdY) {
+        posX = x;
+        posY = y;
+        color = c;
+        speedX = spdX;
+        speedY = spdY;
+        batch = null;
+    }
+}
