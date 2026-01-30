d11849c0e5dbf74d70ad7b9b2e2b52333ce60802
bransonnng <2500678@sit.singaporetech.edu.sg>
2026-01-30 13:50:07 +0800

    Add files via upload

---
+++ EntityManager.java
@@ -0,0 +1,50 @@
+package io.github.some_example_name.lwjgl3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EntityManager {
+	private Array<Entity> entityList;
+	
+	// add entity
+	public void addEntity(Entity entity) {
+		entityList.add(entity);
+	}
+	
+	// remove entity
+	public void removeEntity(Entity entity, boolean identity) {
+		entityList.removeValue(entity, identity);
+	}
+	
+	// get entity list
+	public Array<Entity> getEntityList() {
+		return entityList;
+	}
+	
+	// update entity list
+	public void updateList() {
+		for (Entity entity : entityList) {
+			entity.update();
+		}
+	}
+	
+	// allow entity to move with their own speeds by calling movement function
+	public void movement(float speed) {
+		for (Entity entity : entityList) {
+			entity.movement(speed);
+		}
+	}
+	
+	// draw entity
+	public void draw(ShapeRenderer shape) {
+		for (Entity entity : entityList) {
+			entity.draw(shape);
+		}
+	}
+	
+	// constructor for entity list
+	public EntityManager() {
+		entityList = new Array<Entity>();
+	}
+
+}
