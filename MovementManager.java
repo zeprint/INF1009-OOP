6af6e0090d79e94ce2e3d2e884b98acd40e81cc2
Aden <adenseow.sg@gmail.com>
2026-02-01 00:29:56 +0800

    replaced classes that were using java to libgdx

---
+++ MovementManager.java
@@ -0,0 +1,60 @@
+package io.github.some_example_name.lwjgl3;
+
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.graphics.Color;
+
+/** Movement Manager class for movement component lifecycle and updates (SRP: Component management) */
+public class MovementManager {
+    private Array<MovementComponent> components;
+    
+    /** Initializes empty list of movement components */
+    public MovementManager() {
+        this.components = new Array<>();
+    }
+    
+    /** Register a movement component with MovementManager */
+    public void registerComponent(MovementComponent component) {
+        if (component != null && !components.contains(component, true)) {
+            components.add(component);
+        }
+    }
+    
+    /** Remove movement component */
+    public void unregisterComponent(MovementComponent component) {
+        components.removeValue(component, true);
+    }
+    
+    /** Get a specific entity associated with the registered component */
+    public MovementComponent getComponent(Entity entity) {
+        for (MovementComponent component : components) {
+            if (component.getEntity() == entity) {
+                return component;
+            }
+        }
+        return null;
+    }
+    
+    /** Update all registered movement components */
+    public void update(float deltaTime) {
+        for (MovementComponent component : components) {
+            if (component.isEnabled()) {
+                component.update(deltaTime);
+            }
+        }
+    }
+    
+    /** Total number of registered components */
+    public int getComponentCount() {
+        return components.size;
+    }
+    
+    /** Remove all registered components */
+    public void clearComponents() {
+        components.clear();
+    }
+    
+    /** Array list of all registered components */
+    public Array<MovementComponent> getAllComponents() {
+        return new Array<>(components);
+    }
+}
