eb5343b9bccb5dfa0b0c8ecb8901cc35a8aa27f5
DT-77 <107479299+DT-77@users.noreply.github.com>
2026-02-13 00:59:33 +0800

    Refactor Scene class to include camera and viewport

--- Scene.java
+++ Scene.java
@@ -1,71 +1,53 @@
 package io.github.some_example_name.lwjgl3;
 
-// Abstract Scene class
-// Base class for all scenes in the game
-public abstract class Scene {
-    protected String name;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public abstract class Scene implements ISceneSystem {
+
+    protected OrthographicCamera camera;
+    protected Viewport viewport;
+    protected SceneManager sceneManager;
     protected boolean isPaused;
-    protected EntityManager entityManager;
 
-    // Constructor for Scene
     public Scene() {
-        this.name = "";
-        this.isPaused = false;
-        this.entityManager = new EntityManager();
-    }
+        camera = new OrthographicCamera();
+        // Standard virtual resolution 800x600
+        viewport = new FitViewport(800, 600, camera);
+        viewport.apply();
 
-    // Initialize the scene
-    // @return true if successful, false otherwise
-    public abstract boolean create();
-
-    // Update the scene
-    // @param dt Delta time in seconds
-    // @return true if successful, false otherwise
-    public abstract boolean update(float dt);
-
-    // Render the scene
-    // @return true if successful, false otherwise
-    public abstract boolean render();
-
-    // Dispose of scene resources
-    // @return true if successful, false otherwise
-    public abstract boolean dispose();
-
-    // Pause the scene
-    // @return true if successful, false otherwise
-    public boolean pause() {
-        isPaused = true;
-        return true;
-    }
-
-    // Resume the scene
-    // @return true if successful, false otherwise
-    public boolean resume() {
+        // Center camera
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
         isPaused = false;
-        return true;
     }
 
-    // Get the scene name
-    // @return Scene name
-    public String getName() {
-        return name;
+    public void setSceneManager(SceneManager sm) {
+        this.sceneManager = sm;
     }
 
-    // Set the scene name
-    // @param name New scene name
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
     }
 
-    // Check if scene is paused
-    // @return true if paused, false otherwise
-    public boolean isPaused() {
-        return isPaused;
+    @Override
+    public void pause() {
+        isPaused = true;
     }
 
-    // Get the entity manager
-    // @return EntityManager instance
-    public EntityManager getEntityManager() {
-        return entityManager;
+    @Override
+    public void resume() {
+        isPaused = false;
+    }
+
+    // Default empty implementation for create/dispose if not overridden
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void dispose() {
     }
 }
