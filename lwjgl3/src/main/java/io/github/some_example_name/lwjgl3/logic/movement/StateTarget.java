a2d3c16bdd6cab6382595af8bd9608ee64915bb2
Aden <adenseow.sg@gmail.com>
2026-03-21 23:17:40 +0800

    delete state target

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/logic/movement/StateTarget.java
+++
@@ -1,8 +0,0 @@
-package io.github.some_example_name.lwjgl3.logic.movement;
-
-// Toggleable state - used by Dodge Movement to turn dodge state on/off while the key is held
-
-public interface StateTarget {
-
-    void setStateActive(boolean active);
-}
